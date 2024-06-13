f250a9223a1cfd96210f27c08ac5fa9b4bad1632
danaosar <rachmanpradana31@gmail.com>
2024-06-13 17:14:37 +0700

    Man-s Speedshop

---
+++ application/controllers/kategori.php
@@ -0,0 +1,39 @@
+<?php
+
+class Kategori extends CI_Controller{
+    public function ShockBreaker()
+    {
+        $data['shockbreaker'] = $this->model_kategori->data_shockbreaker()->result();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('shockbreaker', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Velg()
+    {
+        $data['velg'] = $this->model_kategori->data_velg()->result();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('velg', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Mesin()
+    {
+        $data['mesin'] = $this->model_kategori->data_mesin()->result();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('mesin', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Aksesoris()
+    {
+        $data['aksesoris'] = $this->model_kategori->data_aksesoris()->result();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('aksesoris', $data);
+        $this->load->view('templates/footer');
+    }
+}
---
+++ application/models/model_kategori.php
@@ -0,0 +1,19 @@
+<?php
+
+class Model_Kategori extends CI_Model{
+    public function data_shockbreaker(){
+      return $this->db->get_where("tb_barang", array('kategori' => 'shockbreaker'));
+    }
+
+    public function data_velg(){
+      return $this->db->get_where("tb_barang", array('kategori' => 'velg'));
+    }
+
+    public function data_mesin(){
+      return $this->db->get_where("tb_barang", array('kategori' => 'mesin'));
+    }
+
+    public function data_aksesoris(){
+      return $this->db->get_where("tb_barang", array('kategori' => 'aksesoris'));
+    }
+}
